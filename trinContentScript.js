69a07e508752a578856b098cc9da6b91a1c96889
Eklavya Sharma <ekurgn@gmail.com>
2023-03-19 22:33:36 -0500

    don't load from extension if already loaded

--- trinContentScript.js
+++ trinContentScript.js
@@ -1,12 +1,36 @@
-const trinPath = chrome.runtime.getURL("trin.js");
-import(trinPath).then((trin) => {
-    window.trin = trin;
-    const foundScripts = trin.detectScripts(document.body.innerText);
-    if(foundScripts.size > 0) {
-        console.log('trin found:', Array.from(foundScripts).map((script) => script.name).join(', '));
-        trin.initUI();
+function isAlreadyLoaded() {
+    if(window.hasOwnProperty('trin')) {
+        return true;
     }
     else {
-        console.log("trin didn't find any Indian scripts.");
+        const jscripts = document.getElementsByTagName('script');
+        for(const jscript of jscripts) {
+            if(jscript.src.endsWith('trin.js')) {
+                return true;
+            }
+        }
     }
-});
+    return false;
+}
+
+function importAndInitUI() {
+    const trinPath = chrome.runtime.getURL("trin.js");
+    import(trinPath).then((trin) => {
+        window.trin = trin;
+        const foundScripts = trin.detectScripts(document.body.innerText);
+        if(foundScripts.size > 0) {
+            console.log('trin detected ', Array.from(foundScripts).map((script) => script.name).join(', '));
+            trin.initUI();
+        }
+        else {
+            console.log("trin didn't find any Indian scripts.");
+        }
+    });
+}
+
+if(isAlreadyLoaded()) {
+    console.log('trin already loaded.');
+}
+else {
+    importAndInitUI();
+}
